099f9cc721b8901515b3bd4a5402dd212abfdab7
lucas-faleiro <lucas.faleirosilva@gmail.com>
2025-12-09 20:13:11 -0300

    Add: getRecipeById fucntion

--- src/data/api.js
+++ src/data/api.js
@@ -2,6 +2,8 @@ const SEAFOOD_URL =
   "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood";
 const SEARCH_INPUT_URL =
   "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+const RECIPE_BY_ID_URL =
+  "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
 
 export const getRecipes = async (filter) => {
   try {
@@ -16,5 +18,16 @@ export const getRecipes = async (filter) => {
     return data.meals;
   } catch (error) {
     console.error("Erro ao recuperar as receitas:", error);
+  }
+};
+
+export const getRecipeById = async (id) => {
+  try {
+    const response = await fetch(`${RECIPE_BY_ID_URL}${id}`);
+    const data = await response.json();
+    console.log(data.meals);
+    return data.meals;
+  } catch (error) {
+    console.error("Erro ao recuperar a receita:", error);
   }
 };
